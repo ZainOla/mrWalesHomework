6f8ff6d62286ebcaf722f984a5a9427a142183e4
Zainab Olaniyan <zainabolaniyan1@gmail.com>
2024-03-27 09:46:15 +0100

    My assignment

---
+++ practise.js
@@ -0,0 +1,4 @@
+
+let str="  The morning is upon us  "
+console.log(str.trim().substring(2,11))
+console.log(str.trim().slice(2,11))
---
+++ test.js
@@ -0,0 +1,151 @@
+// let ans= "abu abu abu"
+// //let secondoccur= ans.slice(11,21)
+// //console.log(secondoccur)
+
+// let ans1=ans.indexOf(" ")
+// console.log(ans1)
+// let ans2=ans.indexOf(' ',ans1+1)
+// console.log(ans2)
+// let secondWord = ans.slice(ans1+1,ans2)
+// console.log(secondWord)
+
+//let wake= "  The morning is upon us  " 
+//let result= wake.trim()
+//result=result.substring(2,11)
+//console.log(result)
+
+
+//QUESTION
+//create a voting system that allows 
+//citizens to vote if their age is 18 and above.
+//Display the message of your choice to the citizens.
+
+
+//IF, ELSE IF
+//let voteAge = 12
+//if (voteAge >= 18) {
+//console.log ("your age is". concat(" ", voteAge))
+//console.log ('proceed to vote')
+//} else {
+//console.log ("no voting")
+//}
+//console.log('Any other input')
+
+// let voteAge = 12
+// if (voteAge >= 18) {
+//     console.log ("no voting")
+// } else {
+
+//     console.log ("your age is". concat(" ", voteAge))
+//     console.log ('proceed to vote')}
+// console.log('Any other input')
+
+
+// IF, ELSE
+//let votingAge=18-         this is the pegged age
+//if(10 >= votingAge) { -   this is my own age
+//  console.log("congratulations! You are eigible to vote")
+//} else{
+// console.log("sorry! you are not eligible")
+//}
+
+
+// IF, ELSE
+// let studentScore= 30
+// if(studentScore <= 30 ) {
+// console. log("Grade F")
+// }
+// else{
+// console.log("Promoted")
+// }
+
+// IF, ELSE IF, ELSE
+// let studentScore = 100
+// if (studentScore <= 30 ){
+// console.log("Grade F")
+// }
+// else if (studentScore <= 50){
+// console.log("Grade D")
+// }
+// else if(studentScore <= 60){
+// console.log("Grade C")
+// }
+// else if(studentScore <= 70){
+// console.log("Grade B")
+// }
+// else{
+// console.log("Promoted")
+// }
+
+
+
+// let str="  The morning is upon us  "
+// console.log(str.trim().substring(2,11))
+// console.log(str.slice(2,11))
+
+// MR WALE'S THIRD ASSIGNMENT
+
+//QUESTION 1
+// let number= 4+4
+// if(number>0){
+//     console.log("Positive")
+// }
+// else if(number<0){
+//     console.log("Negative")
+// }
+// else{
+//     console.log("Zero")
+// }
+
+
+//QUESTION 2
+// let number=10
+// if(number%2==0){
+//     console.log("Even")
+// }
+// else{
+//     console.log("Odd")
+// }
+// 
+
+//QUESTION 3
+// let age=15
+// if(age%5==0){
+//     console.log("Multiple of 5")
+// }
+// else if(age%3==0){
+//     console.log("Multiple of 3")
+// }
+// else{
+//     console.log("Neither")
+// }
+
+//QUESTION 4
+// let year=2300
+// if(year >=1901 && year <=2000){
+//     console.log("20th century")
+// }
+// else if(year >=2001 && year <=2100){
+//     console.log("21st century")
+// }
+// else if(year >=2101 && year <=2200){
+//     console.log("22nd century")
+// }
+// else{
+//     console.log("Not an allocated century")
+// }
+
+//QUESTION 5
+// let value=49
+// if(value < 10){
+//     console.log("0")
+// }
+// else if(value >=10 && value <=50){
+//     console.log("1")
+// }
+// else if(value >=51 && value <=100){
+//     console.log("2")
+// }
+// else{
+//     console.log("3")
+// }
